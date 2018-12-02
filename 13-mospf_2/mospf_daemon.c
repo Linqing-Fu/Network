79b1604fc3e90c7ffbb2578827c00c5b099d05c1
qingzenholiu <lynn6868@163.com>
2018-12-02 21:19:39 +0800

    finished

--- 13-mospf_2/mospf_daemon.c
+++ 13-mospf_2/mospf_daemon.c
@@ -5,6 +5,7 @@
 
 #include "ip.h"
 #include "packet.h"
+#include "rtable.h"
 
 #include "list.h"
 #include "log.h"
@@ -13,10 +14,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdbool.h>
+
+#define MAX_NODE 10
+#define MAX_LEN 12
 
 extern ustack_t *instance;
 
+struct iface_gw{
+	iface_info_t *iface;
+	u32 gw;
+};
+
+u32 rid_array[MAX_NODE];
+int dist[MAX_NODE];
+bool visited[MAX_NODE];
+int prev[MAX_NODE];
+
+
 pthread_mutex_t mospf_lock;
+
 
 void mospf_init()
 {
@@ -43,6 +60,21 @@ void *sending_mospf_lsu_thread(void *param);
 void *checking_nbr_thread(void *param);
 void send_mospf_lsu_once();
 void *print_nbr(void *param);
+void *checking_database_thread(void * param);
+
+void *generate_rtable();
+int node_num();
+void generate_graph(int my_graph[MAX_NODE][MAX_NODE], int num);
+void calculate_shortest_path(int my_graph[MAX_NODE][MAX_NODE], int num);
+void transform_to_rtable(int num);
+void print_graph(int my_graph[MAX_NODE][MAX_NODE], int num);
+int find_rid_index(u32 rid, int num);
+int min_dist(int *dist, bool *visited, int num);
+mospf_db_entry_t *find_db_entry(u32 rid);
+bool check_whether_in_rtable(u32 subnet);
+int find_next_hop(u32 rid, int num);
+struct iface_gw *find_iface_gw(u32 rid);
+
 
 
 
@@ -72,11 +104,44 @@ void *print_nbr(void *param) {
 
 void mospf_run()
 {
-	pthread_t hello, lsu, nbr, dump;
+	pthread_t hello, lsu, nbr, dump, g_rtable, update;
 	pthread_create(&hello, NULL, sending_mospf_hello_thread, NULL);
 	pthread_create(&lsu, NULL, sending_mospf_lsu_thread, NULL);
 	pthread_create(&nbr, NULL, checking_nbr_thread, NULL);
-	pthread_create(&dump, NULL, print_nbr, NULL);
+	// pthread_create(&dump, NULL, print_nbr, NULL);
+	pthread_create(&g_rtable, NULL, generate_rtable, NULL);
+	pthread_create(&update, NULL, checking_database_thread, NULL);
+
+}
+
+void *checking_database_thread(void * param) {
+    time_t now = 0;
+    rt_entry_t * rt_entry = NULL, * rt_entry_q = NULL;
+    mospf_db_entry_t * db_entry = NULL, * db_entry_q = NULL;
+    while (1) {
+		// printf("checking database...\n");
+        pthread_mutex_lock(&mospf_lock);
+        if (!list_empty(&mospf_db)) {
+            now = time(NULL);
+            list_for_each_entry_safe(db_entry, db_entry_q, &mospf_db, list) {
+                if((now - db_entry->alive) >= 35){
+                    list_for_each_entry_safe(rt_entry, rt_entry_q, &rtable, list) {
+                        if(rt_entry->gw != 0)
+                            remove_rt_entry(rt_entry);
+                    }    
+
+                    list_delete_entry(&(db_entry->list));
+                    free(db_entry);
+                }
+            }
+        } else{
+            // printf("Database is now empty.\n");
+        }
+        // printf("exit checking database\n");
+        pthread_mutex_unlock(&mospf_lock);
+        sleep(1);
+    }
+    return NULL;
 }
 
 void *sending_mospf_hello_thread(void *param)
@@ -315,6 +380,7 @@ void handle_mospf_lsu(iface_info_t *iface, char *packet, int len)
 					p->seq = ntohs(m_lsu->seq);
 					p->rid = ntohl(mospf->rid);
 					p->nadv = ntohl(m_lsu->nadv);
+					p->alive = time(NULL);
 					for(int i = 0; i < p->nadv; i++){
 						p->array[i].subnet = ntohl(m_lsa->subnet);
 						p->array[i].mask = ntohl(m_lsa->mask);
@@ -333,6 +399,7 @@ void handle_mospf_lsu(iface_info_t *iface, char *packet, int len)
 		new->seq = ntohs(m_lsu->seq);
 		new->rid = ntohl(mospf->rid);
 		new->nadv = ntohl(m_lsu->nadv);
+		new->alive = time(NULL);
 
 		new->array = (struct mospf_lsa *)malloc(sizeof(struct mospf_lsa) * (new->nadv));
 		for(int i = 0; i < new->nadv; i++){
@@ -376,10 +443,253 @@ void handle_mospf_lsu(iface_info_t *iface, char *packet, int len)
 		}
 	}
 	fprintf(stdout, "exit: handle mOSPF LSU message.\n");
+}
 
+
+void *generate_rtable(){
+	int my_graph[MAX_NODE][MAX_NODE];
+
+	while(1){
+		sleep(5);
+		pthread_mutex_lock(&mospf_lock);
+		for(int i = 0; i < MAX_NODE; i++){
+			rid_array[i] = 0;
+        	dist[i] = 0;
+			visited[i] = false;
+			prev[i] = 0;
+		}
+		int num = node_num();
+		printf("num%d\n", num);
+
+		//init
+		for(int i = 0; i < num; i++){
+			for(int j = 0; j < num; j++){
+				my_graph[i][j] = (i == j)?0:MAX_LEN;
+			}
+		}
+
+		// printf("aaaaaaaaaaaaaa\n");
+		generate_graph(my_graph, num);
+		// printf("bbbbbbbbbbbbbbbbbbb\n");
+		print_graph(my_graph, num);
+		// printf("ccccccccccccccccccc\n");
+		calculate_shortest_path(my_graph, num);
+		// printf("ddddddddddddddddddd\n");
+		transform_to_rtable(num);
+		// printf("fffffffffffffffffff\n");
+		print_rtable();
+		pthread_mutex_unlock(&mospf_lock);
+
+
+	}
+	return NULL;
+}
+
+
+int node_num(){
+	int num = 1;
+	mospf_db_entry_t *p = NULL;
+	if(!list_empty(&mospf_db)){
+		list_for_each_entry(p, &mospf_db, list){
+			rid_array[num] = p->rid;
+			num++;
+		}
+	}
+	rid_array[0] = instance->router_id;
+	return num;
+}
+
+void generate_graph(int my_graph[MAX_NODE][MAX_NODE], int num){
+
+	//update neighbor path to 1
+	mospf_db_entry_t *p, *q;
+	int index = 0;  //index 0: self router
+	int y = 0;
+	if(!list_empty(&mospf_db)){
+		list_for_each_entry_safe(p, q, &mospf_db, list){
+			for(int i = 0; i < p->nadv; i++){
+				printf("p->array rid:%u\n", p->array[i].rid);
+				y = find_rid_index(p->array[i].rid, num);
+				index = find_rid_index(p->rid, num);
+				printf("y:%d\n", y);
+				if(y == -1){
+					continue;
+				}
+				my_graph[index][y] = 1;
+				my_graph[y][index] = 1;
+			}
+		}
+	}
+}
+
+
+void calculate_shortest_path(int my_graph[MAX_NODE][MAX_NODE], int num){
+	//dijkstra algorithm
+
+	for(int i = 0; i < num; i++){
+		dist[i] = my_graph[0][i];
+		visited[i] = false;
+		prev[i] = (dist[i] == MAX_LEN || dist[i] == 0)?-1:0;
+	}
+	visited[0] = true;
+
+	int u_index;
+
+	for(int i = 1; i < num; i++){
+		u_index = min_dist(dist, visited, num);
+		visited[u_index] = true;
+
+		for(int j = 0; j < num; j++){
+			if((visited[j] == false) && (my_graph[u_index][j] > 0) && (dist[u_index] + my_graph[u_index][j] < dist[j]) && dist[u_index]!=MAX_LEN){
+				dist[j] = dist[u_index] + my_graph[u_index][j];
+				prev[j] = u_index;
+			}
+		}
+	}
 
 }
 
+void transform_to_rtable(int num){
+	//sort
+	int temp1, temp2;
+	int index[num];
+	for(int i = 0; i < num; i++){
+		index[i] = i;
+	}
+	for(int i = 0; i < num - 1; i++){
+		for(int j = 0; j < num - i - 1; j++){
+			if(dist[j] > dist[j + 1]){
+				temp1 = dist[j];
+				dist[j] = dist[j + 1];
+				dist[j + 1] = temp1;
+				temp2 = index[j];
+				index[j] = index[j + 1];
+				index[j + 1] = temp2;
+			}
+		}
+	}
+
+	mospf_db_entry_t *p = NULL, *q = NULL;
+	bool in;
+	rt_entry_t *new = NULL;
+	struct iface_gw *result;
+	int next_hop;
+
+	
+	for(int i = 0; i < num; i++){
+		if(prev[index[i]] != -1 && !list_empty(&mospf_db)){
+			list_for_each_entry_safe(p, q, &mospf_db, list){
+				// p = find_db_entry(rid_array[index[i]]);
+				for(int j = 0; j < p->nadv; j++){
+					in = check_whether_in_rtable(p->array[j].subnet);
+					if(in == false){
+						next_hop = find_next_hop(p->rid, num);
+						result = find_iface_gw(rid_array[next_hop]);
+						// printf("111\n");
+						new = new_rt_entry(p->array[j].subnet, result->iface->mask, result->gw, result->iface);
+						// printf("222\n");
+						add_rt_entry(new);
+						// printf("333\n");
+					}
+				}
+			}
+		}
+	}
+	
+}
+
+void print_graph(int my_graph[MAX_NODE][MAX_NODE], int num){
+	for(int i = 0; i < num; i++){
+		printf("%u\t", rid_array[i]);
+	}
+	printf("\n");
+	for(int i = 0; i < num; i++){
+		for(int j = 0; j < num; j++){
+			printf("%d\t", my_graph[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
+int find_rid_index(u32 rid, int num){
+	for(int i = 0; i < num; i++){
+		if(rid_array[i] == rid){
+			return i;
+		}
+	}
+	printf("error: cannot find rid\n");
+	return -1;
+}
+
+
+int min_dist(int *dist, bool *visited, int num){
+	int min = MAX_LEN;
+	int node_index = -1;
+	for(int i = 0; i < num; i++){
+		if(visited[i] == false && dist[i] < min){
+			min = dist[i];
+			node_index = i;
+		}
+	}
+	return node_index;
+}
+
+
+
+mospf_db_entry_t *find_db_entry(u32 rid){
+	mospf_db_entry_t *p;
+	list_for_each_entry(p, &mospf_db, list){
+		if(p->rid == rid){
+			return p;
+		}
+	}
+	printf("error:cannot find db_entry rid:%u\n", rid);
+	return NULL;
+}
+
+bool check_whether_in_rtable(u32 subnet){
+	rt_entry_t *p;
+	list_for_each_entry(p, &rtable, list){
+		if(p->dest == subnet){
+			return true;
+		}
+	}
+	return false;
+}
+
+int find_next_hop(u32 rid, int num){
+	int next_hop = find_rid_index(rid, num);
+	while(prev[next_hop] != 0){
+		next_hop = prev[next_hop]; 
+	}
+	printf("next hop:%d\n", next_hop);
+	return next_hop;
+}
+
+struct iface_gw *find_iface_gw(u32 rid){
+	printf("find iface & gw\n");
+	iface_info_t *iface;
+	mospf_nbr_t *p;
+	int find = 0;
+	struct iface_gw *result = (struct iface_gw *)malloc(sizeof(struct iface_gw));
+
+	list_for_each_entry(iface, &(instance->iface_list), list){
+		list_for_each_entry(p, &(iface->nbr_list), list){
+			if(rid == p->nbr_id){
+				result->iface = iface;
+				result->gw = p->nbr_ip;
+				find = 1;
+				break;
+			}
+		}
+		if(find == 1){
+			break;
+		}
+	}
+	printf("exit find iface & gw\n");
+	return result;
+}
 
 
 void handle_mospf_packet(iface_info_t *iface, char *packet, int len)
